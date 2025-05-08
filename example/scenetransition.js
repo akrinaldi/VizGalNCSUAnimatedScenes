cbb581876a7e7555a5119f19c8b7996486c623dc
Bee <35223187+akrinaldi@users.noreply.github.com>
2025-05-08 14:42:29 -0400

    Add files via upload

---
+++ example/scenetransition.js
@@ -0,0 +1,41 @@
+ let currentScene = 1;
+  
+function showScene(sceneNumber) {
+    // Hide scenes on initialization
+    document.getElementById('scene1').style.display = 'none';
+    document.getElementById('scene2').style.display = 'none';
+    document.getElementById('scene3').style.display = 'none';
+    document.getElementById('scene4').style.display = 'none';
+
+  
+    // Show the chosen scene
+    if (sceneNumber === 1) {
+      document.getElementById('scene1').style.display = 'block';
+    } else if(sceneNumber===2) {
+      document.getElementById('scene2').style.display = 'block';
+    } else if(sceneNumber===3) {
+      document.getElementById('scene3').style.display = 'block';
+    } else if(sceneNumber===4) {
+      document.getElementById('scene4').style.display = 'block';
+    }
+  }
+
+  // start the display
+  showScene(currentScene);
+  
+
+  // set scene array for looping
+  const scenes = [1, 2, 3, 4];
+  let currentSceneIndex = 0;
+  
+  // function to move to next scene
+  function nextScene() {
+    currentSceneIndex = (currentSceneIndex + 1) % scenes.length;
+    currentScene = scenes[currentSceneIndex];
+    console.log(currentScene);
+    showScene(currentScene);
+}
+  // function to change scenes every set interval
+  setInterval(nextScene, 5000);
+
+  
